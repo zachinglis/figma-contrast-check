d0909082999e5f5c613202a2903650d5cfbc5763
Zach Inglis <zach@lt3media.com>
2022-06-29 16:34:23 +0100

    Version 1

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Constrast Check
 
+A simple contrast checker widget to put in to your styleguides.
 
+Click the widget to input the foreground color. The background color is inferred by the parent frame. There are options to test against: AA, AAA, Large Text, Normal Text.
 
-## Notes
-- It only works on direct parent
-- It only takes the last background.
+(This was a quick and dirty plugin built in a day. So please excuse anything that's not perfect and feel free to make PRs.) It definitely needs a refactor and cleanup.
+
+## Building
+
+To build run `npm run build`
+
+## Future Plans
+- Close Pass / Close Fail
+
+## Authors
+- Zach Inglis - [Twitter](https://twitter.com/zachinglis)
+
+## With Thanks
+- https://remixicon.com/
+- https://css-tricks.com/converting-color-spaces-in-javascript/
+- https://blog.cristiana.tech/calculating-color-contrast-in-typescript-using-web-content-accessibility-guidelines-wcag
+- https://dev.to/alvaromontoro/building-your-own-color-contrast-checker-4j7o
--- code.tsx
+++ code.tsx
@@ -1,18 +1,81 @@
-// This is a counter widget with buttons to increment and decrement the number.
+// Large
+// Min: 18.6px + bold and heavier
+// Or, Min: 24px
+// Normal is else
+
+const ratiosToCompare = {
+  'AA': {
+    large: 1/3,
+    normal: 1/4.5,
+  },
+  'AAA': {
+    large: 1/4.5,
+    normal: 1/7,
+  },
+}
+
+interface checkContrastArgs {
+  foregroundColor?: string
+  ratioLevel?: string
+  fontSize?: string
+}
 
 const { widget, notify } = figma
-const { useSyncedState, usePropertyMenu, AutoLayout, Text, Input, useWidgetId } = widget
+const { useSyncedState, usePropertyMenu, AutoLayout, Rectangle, SVG, Text, Input, useWidgetId } = widget
 
 function Widget() {
-  const [currentColor, setCurrentColor] = useSyncedState('currentColor', '#000000')
-  const [parentColor, setParentColor] = useSyncedState('parentColor', '#ffffff')
+  //#section Initial State
+  const [foregroundColor, setForegroundColor] = useSyncedState('foregroundColor', '#000000')
+  const [backgroundColor, setBackgroundColor] = useSyncedState('parentColor', '#ffffff')
+  const [ratio, setRatio] = useSyncedState('ratio', '0')
+  const [passes, setPasses] = useSyncedState('passes', 'false')
 
+  const [optionRatioLevel, setOptionRatioLevel] = useSyncedState('optinRatioLevel', 'AA')
+  const [optionFontSize, setOptionFontSize] = useSyncedState('optionFontSize', 'large')
+  const [optionSize, setOptionSize] = useSyncedState('optionSize', 'large')
+  const [settingsOpened, setSettingsOpened] = useSyncedState('settignsOpened', false)
+  
   const id = useWidgetId()
+  //#endregion
+  //#region Helper methods - Colors & Contrast
+  function normalisedRGB(rgb: RGB): RGB {
+    return {
+      r: Math.round(rgb.r * 255),
+      g: Math.round(rgb.g * 255),
+      b: Math.round(rgb.b * 255),
+    }
+  }
+
+  function luminance(rgb: RGB) {
+    const [r, g, b] = [rgb.r, rgb.g, rgb.b].map((v) => {
+        v /= 255;
+        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+    });
+    return r * 0.2126 + g * 0.7152 + b * 0.0722;
+  };
+
+  function contrast(colorA: RGB, colorB: RGB) {
+    const foregroundLumiance = luminance(colorA);
+    const backgroundLuminance = luminance(colorB);
+    return backgroundLuminance < foregroundLumiance
+        ? ((backgroundLuminance + 0.05) / (foregroundLumiance + 0.05))
+        : ((foregroundLumiance + 0.05) / (backgroundLuminance + 0.05));
+  };
+
+  function HexToRGB(hex: string): RGB {
+    hex = hex.slice(1);
+    const value = parseInt(hex, 16);
+    const r = (value >> 16) & 255;
+    const g = (value >> 8) & 255;
+    const b = value & 255;
+    return { r, g, b };
+  };
 
   function RGBToHex(rgb: RGB): string {
-    let r = Math.round(rgb.r * 255).toString(16);
-    let g = Math.round(rgb.g * 255).toString(16);
-    let b = Math.round(rgb.b * 255).toString(16);
+    let normalised = normalisedRGB(rgb)
+    let r = normalised.r.toString(16);
+    let g = normalised.g.toString(16);
+    let b = normalised.b.toString(16);
   
     if (r.length == 1)
       r = "0" + r;
@@ -23,38 +86,96 @@ function Widget() {
   
     return "#" + r + g + b;
   }
-  
-  function testContrast() {
-    const node = figma.getNodeById(id) as WidgetNode
-    const parent = node.parent as FrameNode
-    
+  //#endregion
+  //#region Helper methods - Nodes
+  function getParentFrame(node: WidgetNode): FrameNode | undefined {
+    if(!node.parent) return
+
+    if(node.parent.type === 'FRAME') {
+      return node.parent as FrameNode
+    } else {
+      return getParentFrame(node.parent as WidgetNode)
+    }
+  }
+
+  function getParentFrameHex(node: WidgetNode): string | undefined {
+    const parent = getParentFrame(node)
+
     if(!parent) {
-      notify("Parent is not a frame")
+      notify("Widget must be inside a frame")
       return
     }
+
     const fill = parent.fills[0]
     if(fill === undefined) {
-      notify("Parent needs a fill")
+      notify("Parent frame needs a fill")
       return
     }
     if(fill.type !== 'SOLID') {
-      notify("Parent fill is not a solid color")
+      notify("Parent frame fill is not a solid color")
       return
     }
+  
+    return RGBToHex(fill.color)
+  }
+  //#endregion
+  
+  function getCurrentRatio(fgColor: string, bgColor:string):number {
+    const ratioContrast = contrast(
+      HexToRGB(fgColor), 
+      HexToRGB(bgColor)
+    )
+    return ratioContrast
+  }
 
-    const bgColor = RGBToHex(fill.color)
-    console.log("fill", fill.color)
-    console.log("bg", bgColor)
-    setParentColor(bgColor)
+  function doesPass(ratio: number, ratioLevel: string, fontSize:string):boolean {
+    const passRatio:number = ratiosToCompare[ratioLevel][fontSize]
+    return ratio < passRatio
+  }
+
+  function setPassOrFail(args: checkContrastArgs = {}) {
+    const node = figma.getNodeById(id) as WidgetNode
+
+    let fgColor = args.foregroundColor || foregroundColor
+    let bgColor = getParentFrameHex(node)
+    let ratioLevel = args.ratioLevel || optionRatioLevel
+    let fontSize = args.fontSize || optionFontSize
+
+    if(!foregroundColor || !backgroundColor) return
+
+    const newRatio = getCurrentRatio(fgColor, bgColor)
+    const newValidity = doesPass(newRatio, ratioLevel, fontSize)
+
+    if(args.foregroundColor) setForegroundColor(fgColor)
+    setBackgroundColor(backgroundColor)
+    setRatio(String(newRatio))
+    setPasses(String(newValidity))
+
+    node.name = `Contrast Checker (${newValidity === true ? 'PASS' : 'FAIL'})`
   }
 
   //#region Settings
-  const [size, setSize] = useSyncedState('size', 'large')
+  const propertyIconColor = "#BCBCBC"
   usePropertyMenu([
     {
-      itemType: 'action',
-      propertyName: 'recalculate',
-      tooltip: 'Recalcuate the contrast',
+      itemType: 'dropdown',
+      tooltip: 'WCAG Level',
+      propertyName: 'ratio',
+      options: [
+        { label: 'AA', option: 'AA' },
+        { label: 'AAA', option: 'AAA' },
+      ],
+      selectedOption: optionRatioLevel
+    },
+    {
+      itemType: 'dropdown',
+      tooltip: 'Size of the text',
+      propertyName: 'text',
+      options: [
+        { label: 'Normal Text', option: 'normal' },
+        { label: 'Large Text', option: 'large' },
+      ],
+      selectedOption: optionFontSize
     },
     {
       itemType: 'separator',
@@ -64,81 +185,142 @@ function Widget() {
     tooltip: 'Change widget size',
     propertyName: 'size',
     options: [
-      { label: 'Tiny', option: 'tiny' },
-      { label: 'Small', option: 'small' },
-      { label: 'Large', option: 'large' },
+      { label: 'Tiny Widget', option: 'tiny' },
+      { label: 'Small Widget', option: 'small' },
+      { label: 'Large Widget', option: 'large' },
     ],
-    selectedOption: 'large'
-  }],
+    selectedOption: optionSize
+  },
+  {
+    itemType: 'separator',
+  },
+  {
+    itemType: 'action',
+    propertyName: 'toggleSettings',
+    tooltip: settingsOpened ? 'Close Settings' : 'Open Settings',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="${propertyIconColor}" d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z"/></svg>`,
+  },  {
+    itemType: 'action',
+    propertyName: 'recalculate',
+    tooltip: 'Recalculate',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="${propertyIconColor}" d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z"/></svg>`,
+  },
+],
   ({propertyName, propertyValue}) => {
     if (propertyName === 'size') {
-      setSize(propertyValue)
+      setOptionSize(propertyValue)
     }
-    if (propertyName === 'recalculate') {
-      testContrast()
+    if (propertyName === 'ratio') {
+      setOptionRatioLevel(propertyValue)
+      setPassOrFail({ratioLevel: propertyValue})
+    }
+    if (propertyName === 'text') {
+      setOptionFontSize(propertyValue)
+      setPassOrFail({fontSize: propertyValue})
+    }
+    if (propertyName === 'toggleSettings') {
+      setSettingsOpened(!settingsOpened)
+    }
+    if(propertyName === 'recalculate') {
+      setPassOrFail()
+      setSettingsOpened(false)
     }
   })
   //#endregion
 
   //#region Widget - currentColor
-  function updateCurrentColor({ characters }) {
-    console.log("editted", characters)
+  function updateForegroundColor({ characters }: TextEditEvent) {
+    setPassOrFail({
+      foregroundColor: characters
+    })
+    closeSettings()
   }
   //#endregion
-// TODO: Width should stay the same
-// TODO: Make color text of widget the chosen color
-// Hover fx
   //#region Visual - Settings Opened
-    const [settingsOpened, setSettingsOpened] = useSyncedState('settignsOpened', false)
-
     function showSettings() { setSettingsOpened(true) }
     function closeSettings() { setSettingsOpened(false) }
   //#endregion
   //#region Visual - Size styles
-  let fontSize = 18
-  let padding = 16
-  if(size === 'tiny') {
-    fontSize = 9
-    padding = 6
+  let widgetFontSize: number = 22
+  let widgetPadding: WidgetJSX.Padding = { horizontal: 16, vertical: 8 }
+  let widgetSpacing: number = 12
+  let widgetWidth: WidgetJSX.Size = 140
+  let widgetHeight: WidgetJSX.Size = 40
+  if(optionSize === 'tiny') {
+    widgetFontSize = 14
+    widgetSpacing = 8
+    widgetPadding = 0
+    widgetHeight = 34
+    widgetWidth = 100
   }
-  if(size == 'small') {
-    fontSize = 13
-    padding = 9
+  if(optionSize == 'small') {
+    widgetFontSize = 16
+    widgetSpacing = 4
+    widgetHeight = 40
+    widgetWidth = 100
+    widgetPadding = { horizontal: 4, vertical: 0 }
   }
+  const widgetBg = "#ffffff"
+  const widgetFg = "#000000"
   //#endregion
 
   return (
     <AutoLayout
-      verticalAlignItems={'center'}
-      spacing={padding / 2}
-      padding={padding}
+      fill={widgetBg}
+      height={widgetHeight}
+      width={widgetWidth}
       cornerRadius={8}
-      fill={'#FFFFFF'}
-      stroke={'#E6E6E6'}
     >
-      {settingsOpened && (
-        <>
+      <Rectangle
+        height={widgetHeight}
+        fill={foregroundColor}
+        stroke="#e4e4e4"
+        strokeWidth={1}
+        strokeAlign="outside"
+        width={10} />
+      <AutoLayout
+        height={widgetHeight}
+        width={widgetWidth - 10}
+        spacing={widgetSpacing}
+        horizontalAlignItems={settingsOpened ? 'end' : 'center'}
+        verticalAlignItems='center'
+      >
+        {settingsOpened && (
           <Input 
-          value={currentColor}
-          placeholder="#000000"
-          width={80}
-          onTextEditEnd={updateCurrentColor} />
-          <Text fontSize={fontSize} onClick={closeSettings}>
-              Close
-          </Text>
-        </>
-      )}
-      {!settingsOpened && (
-        <>
-          <Text fontSize={fontSize} horizontalAlignText={'left'}>
-            Parent: {parentColor} /
-            Current: {currentColor}
-          </Text>
-          <Text fontSize={fontSize} onClick={showSettings}>
-            Change
-          </Text>
-        </>
-      )}
+            placeholder="#000000"
+            value={foregroundColor}
+            width={widgetWidth * 0.8}
+            height={widgetHeight}
+            verticalAlignText='center'
+            fill={widgetFg}
+            fontSize={widgetFontSize} 
+            onTextEditEnd={updateForegroundColor} />
+        )}
+        {!settingsOpened && (
+          <>
+            {passes === 'true' && (
+              <AutoLayout
+                horizontalAlignItems='center'
+                verticalAlignItems='center'
+                spacing={widgetSpacing / 2}
+              >
+                <Text fill={widgetFg} fontWeight="bold" fontSize={widgetFontSize}>Pass</Text>
+                <SVG src={`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="#12C219" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-.997-6l7.07-7.071-1.414-1.414-5.656 5.657-2.829-2.829-1.414 1.414L11.003 16z"/></svg>`} />
+              </AutoLayout>
+            )}
+            {passes !== 'true' && (
+              <AutoLayout
+                horizontalAlignItems='center'
+                verticalAlignItems='center'
+                spacing={widgetSpacing / 2}
+              >
+                <Text fill={widgetFg} fontWeight="bold" fontSize={widgetFontSize}>Fail</Text>
+                <SVG src={`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="#C21212" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-11.414L9.172 7.757 7.757 9.172 10.586 12l-2.829 2.828 1.415 1.415L12 13.414l2.828 2.829 1.415-1.415L13.414 12l2.829-2.828-1.415-1.415L12 10.586z"/></svg>`} />
+              </AutoLayout>
+            )}
+          </>
+        )}
+      </AutoLayout>
     </AutoLayout>
   )
 }
